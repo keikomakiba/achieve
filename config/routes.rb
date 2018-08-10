5cfc1b0238d4e65e5b286697608e22465ed5383c
keiko <keikomakiba@gmail.com>
2018-08-10 04:10:45 +0000

    ad  mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :feeds
   resources :contacts
   root to: 'twitters#top'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807112307) do
+ActiveRecord::Schema.define(version: 20180808134620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20180807112307) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "twitter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
